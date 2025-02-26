fa77480b49ec2828b46d6d70299db963fc79bc7b
RoGoLo-05 <robertogomezlopez2005@gmail.com>
2025-02-26 13:52:28 +0100

    finalizacion de la compra mejorada, ahora descarga el pdf, guarda la orden de la compra y elimina el carrito a la vez para evitar errores, y algunos otros pequeños cambios

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,20 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens; // Agrega esto
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable; // Agrega HasApiTokens aquí
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = ['name', 'email', 'phone', 'password', 'user_type'];
     protected $hidden = ['password', 'remember_token'];
-    
+
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // Agrega esta relación
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
     }
 }
